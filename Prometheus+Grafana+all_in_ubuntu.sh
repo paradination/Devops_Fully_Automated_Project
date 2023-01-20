55f63a0c314d3d90dd6a1f1f77cfedf0ba6f40db
paradination <jjy2k4life@yahoo.com>
2023-01-20 03:26:30 -0600

    grafana and prometheus

---
+++ Prometheus+Grafana+all_in_ubuntu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Hardware requirements: Ubuntu 20.04 instance with mimum t2.micro type instance & port 3000 (grafana), 9100 (node-exporter) should be allowed on the security groups
+sudo apt-get install -y adduser libfontconfig1
+sudo wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
+sudo dpkg -i grafana_7.3.4_amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
+
+# node-exporter installations
+sudo useradd --no-create-home node_exporter
+sudo mkdir ~/prometheus
+cd ~/grafana
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+# setup the node-exporter dependencies
+sudo apt install git
+sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+
+# Setting up prometheus
+sudo useradd --no-create-home prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo mkdir ~/prometheus
+cd ~/prometheus
+
+wget  https://github.com/prometheus/prometheus/releases/download/v2.23.0/prometheus-2.23.0.linux-amd64.tar.gz
+tar -xvf prometheus-2.23.0.linux-amd64.tar.gz
+sudo cp prometheus-2.23.0.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin
+sudo cp -r prometheus-2.23.0.linux-amd64/consoles /etc/prometheus/
+sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
+sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
+
+rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+# setting up the dependencies from the repo setup
+sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/prometheus.yml /etc/prometheus/
+sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/prometheus.service /etc/systemd/system/prometheus.service
+
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+
+
